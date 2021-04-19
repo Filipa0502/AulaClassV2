9d2132dfb7d64a83793977ee071ef470dedd6e82
Filipa <filipadgsillva@gmail.com>
2021-04-19 11:29:20 +0100

    calculadora/home/navbar

--- aluasV2/src/app/app.module.ts
+++ aluasV2/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CalculatorComponent } from './components/calculator/calculator.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CalculatorComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ aluasV2/src/app/components/calculator/calculator.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  valor1 = 0;
+  valor2 = 0;
+  sinal = "";
+  resultado;
+
+  calcula()
+  {
+    //this.resultado = Number(this.valor1) + Number(this.valor2);
+    this.resultado = eval(this.valor1+ this.sinal +this.valor2)
+  }
+
+  ngOnInit(): void {
+  }
+
+}
